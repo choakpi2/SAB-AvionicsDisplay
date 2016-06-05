4782b0bfeeef4558e4723381aa4f375360205be9
Patchara Choakpichitchai <Sunnoopy@Patcharas-MacBook-Air.local>
2016-06-05 21:19:24 +0700

    Aircraft.py created

---
+++ Aircraft.py
@@ -0,0 +1,16 @@
+#AIRCRAFT OBJECT
+#This contains the necessary class methods required to pull data from the glider
+#Primary Flight Data (PFD)
+#Airspeed
+#Attitude
+#VSI
+#ASI
+#
+
+class Aircraft(object):
+	def __init__(self)
+		self.ASI = 800
+		self.VSI = 800
+		self.Altitude = 900
+
+	def 
