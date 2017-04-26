fddc80ec9d4af281a0390862b77692709755187c
choakpi2 <patchara_choak@hotmail.com>
2017-04-26 18:33:47 -0500

    Mac-Vim: Created a deployed version of Vision with drivers

---
+++ software/gui/VisionDeployed/Vision.py
@@ -0,0 +1,208 @@
+import sys
+import os
+from OpenGL import GL
+from OpenGL import GLU
+from OpenGL import GLUT
+import pygame
+from pygame import locals
+from pygame import image
+import time
+import guage
+
+# This is code to import config file (config.py)
+try:
+    import config
+except ImportError:
+    # We're in a py2exe, so we'll append an element to the (one element)
+    # sys.path which points to Library.zip, to the directory that contai
+    # Library.zip, allowing us to import config.py
+    # Adds one level up from the Library.zip directory to the path, so import will go forward
+    sys.path.append(os.path.split(sys.path[0])[0])
+    import config
+
+
+class screen_c(object):
+    # This controls what is in each screen.
+    def __init__(self, x, guage_list):
+        self.guage_list = guage_list  # list of guages to cycle through
+        self.guage_index = 0
+        self.x = x
+        self.y = 20
+        self.width = 480
+        self.heigth = 800
+
+    def cycle(self):
+        self.guage_index += 1
+        if self.guage_index >= len(self.guage_list):
+            self.guage_index = 0
+
+    def cycle_reverse(self):
+        self.guage_index -= 1
+        if self.guage_index < 0:
+            self.guage_index = len(self.guage_list) - 1
+
+    def draw(self, aircraft):
+        self.guage_active = self.guage_list[self.guage_index]
+        self.guage_active.draw(aircraft, self.x, self.y)
+
+
+def InitPyGame():
+    GLUT.glutInit(())
+    pygame.init()
+    disp_no = os.getenv("DISPLAY")
+    if disp_no:
+        print "I'm running under X display = {0}".format(disp_no)
+    drivers = ['fbcon', 'directfb', 'svgalib']
+    found = False
+    for driver in drivers:
+        if not os.getenv('SDL_VIDEODRIVER'):
+            os.putenv('SDL_VIDEODRIVER', driver)
+        try:
+            pygame.display.init()
+        except pygame.error:
+            print 'Driver: {0} failed.'.format(driver)
+            continue
+        found = True
+        break
+    if not found:
+        raise Exception('No suitable video driver found!')
+    size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+    print "Framebuffer size: %d x %d" % (size[0], size[1])
+    if config.full_screen:
+        s = pygame.display.set_mode(size, locals.DOUBLEBUF | locals.OPENGL | locals.FULLSCREEN)
+    else:
+        s = pygame.display.set_mode(size, locals.DOUBLEBUF | locals.OPENGL)
+    return s
+
+
+def InitView(smooth, width, heigth):
+    GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
+    GL.glLoadIdentity()
+    GL.glOrtho(0, width, 0.0, heigth, -1.0, 1.0)
+
+    x_s = width/480.0
+    y_s = heigth/800.0
+
+    GL.glScalef(x_s, y_s, 1.0)
+    guage.scissor.x_s = x_s
+    guage.scissor.y_s = y_s
+    if smooth:
+        # Enable Smoothing Antianalising
+        GL.glEnable(GL.GL_LINE_SMOOTH)
+        GL.glEnable(GL.GL_BLEND)
+        # glBlendFunc(GL_SRC_ALPHA, GL_ZERO)
+        GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
+        GL.glHint(GL.GL_LINE_SMOOTH_HINT, GL.GL_DONT_CARE)
+
+
+def DisplaySplash(filename, delay, window_x, window_y):
+    # Display needs to be initialized first.
+    i = image.load(filename)
+    splash_image = guage.bitmap_image(i)
+    # Determine the x and y coords to put in center of screen.
+    splash_x = (window_x / 2) - (splash_image.w/2)
+    splash_y = (window_y / 2) - (splash_image.h/2)
+    GL.glRasterPos3f(splash_x, splash_y, 0)
+    GL.glDrawPixels(splash_image.w, splash_image.h, GL.GL_RGBA, GL.GL_UNSIGNED_BYTE, splash_image.tostring)
+    pygame.display.flip()
+    time.sleep(delay)
+
+
+def DrawWindow(left_screen):
+
+    def draw_nodata(x, y):  # Draw no data text on screen.
+        GL.glColor(guage.red)
+        GL.glLineWidth(5.0)
+        GL.glPushMatrix()
+        GL.glTranslatef(x, y, 0)
+        GL.glScalef(0.4, 0.4, 1.0)
+        guage.glText("NO SIM DATA", 100)
+        GL.glPopMatrix()
+
+    global count
+    left_screen.draw(aircraft_data)
+    # right_screen.draw(aircraft_data)
+    GL.glDisable(GL.GL_SCISSOR_TEST)  # Disable any scissoring.
+    guage.draw_FPS(240, 700, aircraft_data.frame_time)
+    # If Nodata is coming from Flight Sim, show on screen
+    if aircraft_data.nodata:
+        draw_nodata(50, 240)
+    count = count + 1  # Used for FPS calc
+
+
+def main(mode):
+    # global window
+    global starttime
+    global count
+
+    # Start Event Processing Engine
+    starttime = time.time()  # Used for FPS (Frame Per Second) Calculation
+    screen = screen_c(240, [PFD, EICAS1])
+
+    # SETUP KEYBOARD
+    keys = keyboard.keylist(aircraft_data, screen)
+
+    # LOAD GUAGE TEXTURES
+    EICAS1.load_texture()
+
+    while not (aircraft_data.quit_flag):
+        # CLEAR SCREEN
+        GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
+
+        # UPDATE GLOBALTIME
+        aircraft_data.globaltime = time.time()
+        guage.globaltime.update(time.time())
+
+        # DRAW WINDOW
+        DrawWindow(screen)
+
+        # UPDATE SCREEN
+        pygame.display.flip()
+        aircraft_data.test()
+
+        # CHECK KEYPRESS
+        keys.check_events(pygame.event.get(), guage.globaltime.value)
+
+# ===========================================================================
+#  Main program
+# ===========================================================================
+global count
+# global scissor
+guage.scissor.x_s = 1.1  # Just assign these, will be reset later.
+guage.scissor.y_s = 1.0
+count = 0
+x = config.window_x
+y = config.window_y
+
+# INITIALIZE WINDOW
+InitPyGame()
+InitView(True, x, y)
+
+# LOAD SPLASH SCREEN
+if config.splash:
+    DisplaySplash(config.splash_filename, config.splash_delay, x, y)
+
+# IMPORT REST OF MODULES WHILE SPLASH DISPLAYED
+import PFD_mod
+import EICAS1_mod
+import aircraft
+import keyboard
+
+# INITIALIZE GUAGES
+PFD = PFD_mod.PFD_Guage()
+EICAS1 = EICAS1_mod.EICAS1_guage()
+aircraft_data = aircraft.data(PFD)
+
+# RUN MAIN USING CONFIG.PY SETTINGS
+print "Main Loop"
+main(config.mode)
+
+# ===========================================================================
+#  Shuting Down
+# ===========================================================================
+# Close LogFile
+guage.datafile.close()
+# Close pygame mixer
+pygame.mixer.quit()
+# Print average Frames per second on shutdown
+print "FPS ", count / (time.time() - starttime)
---
+++ software/gui/VisionDeployed/config.py
@@ -0,0 +1,64 @@
+#config.py 
+# Config file for PyGlass
+#
+#This imported by PyGlass via  import config
+#
+#CONSTANTS
+TEST = 1 #run test routine (will not recieve any data from Flight Sim program)
+FSXSP0 = 2 #Recieve data from test routine
+FSXSP1 = 3
+FSXSP2 = 4
+ESP = 5
+CLIENT = 6
+#Flight Director Constant type
+Vshape = 0
+LINES = 1
+
+
+# Screen / Window Setup
+#X resolution of window
+window_x = 480
+#Y resolution of window
+window_y = 800
+#Full screen
+full_screen= False
+
+#Set Mode of program
+mode = TEST #Note: case sensitive
+
+#Data directory name (The name of the folder where navdata is stored)
+data_directory = "data"
+sound_directory = "sounds"
+#Splash Screen setup
+splash = True
+splash_filename = 'images/splash.png'
+splash_delay = 0
+#Metric Units
+use_metric_units = False
+#Set max and min lat long values for nav data
+#Only points within these ranges will be stored in the Nav Database
+max_long= -42
+min_long = -180
+max_lat = 72
+min_lat = 10
+#Ranges for moving map
+max_VOR_range = 165.0
+max_NDB_range = 85.0
+max_APT_range = 165.0
+max_FIX_range = 22.0
+min_RWY_length = 4500 #The minimum RWY length to be in airport database.
+#Speed limits  6 flap postitions on RJ (0, 1, 8, 20, 30, 45)
+VNE_flaps = ( 315, 230, 230, 230, 185, 170) 
+VS_flaps = (111, 107, 103, 101, 100, 97)
+Gear_speed_limit = 220
+#Type of Flight Director (Either Vshape or LINES)
+FD_Type = Vshape
+#RA scale Enable
+RA_scale = True
+#Brake Constants  (Estimates for the CRJ-700)
+brake_cooling_CONST = 0.0012
+brake_heating_CONST = 15000 
+brake_sensor_CONST = 0.0073
+#Will output debug file of AP
+debug_AP = False
+logfile = False
